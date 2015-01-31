33e7d2f0ec38e161f612abd89600dd39e77c5713
btracey <tracey.brendan@gmail.com>
2015-01-30 16:04:27 -0800

    added vecxy

--- vecxy.go
+++ vecxy.go
@@ -15,3 +15,23 @@ func (v VecXY) Len() int {
 func (v VecXY) XY(i int) (x, y float64) {
 	return v.X[i], v.Y[i]
 }
+
+type VecXYZ struct {
+	X []float64
+	Y []float64
+	Z []float64
+}
+
+func (v VecXYZ) Len() int {
+	if len(v.X) != len(v.Y) {
+		panic("myplot: length mismatch")
+	}
+	if len(v.X) != len(v.Z) {
+		panic("myplot: length mismatch")
+	}
+	return len(v.X)
+}
+
+func (v VecXYZ) XYZ(i int) (x, y, z float64) {
+	return v.X[i], v.Y[i], v.Z[i]
+}
